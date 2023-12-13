496d369fe2605234c3a3739327567825c199efb4
Sophia <sophiasammartano@icloud.com>
2023-12-13 02:37:35 -0500

    Added non-compressed backups

--- backupsave.bat
+++ backupsave.bat
@@ -5,32 +5,46 @@ cd /d %~dp0
 
 echo Setting variables
 
-:: The directory of your honour mode save game 
-set "backupDir=SAVE DIRECTORY HERE"
+:: The directory of your honour mode save game
+:: Default save directoroy location is %LOCALAPPDATA%\Larian Studios\Baldur's Gate 3\PlayerProfiles\Public\Savegames\Story
+:: The one you're looking for is probably the most recently modified
+set "backupDir=PUT SAVE DIRECTORY HERE"
 echo 	backupDir = %backupDir%
 
-:: The path to WinRAR, if you have 7zip instead ignore this
+:: Set this to true if you don't want to use compression
+set useCompression=true
+echo 	useCompression = %useCompression%
+
+:: If you disabled compression above, feel free to ignore the rest of the settings
+
+:: The path to WinRAR "Rar.exe", if you have 7zip ignore this
 set "rarPath=C:\Program Files\WinRAR\Rar.exe"
 echo 	rarPath = %rarPath%
 
-:: The path to 7zip
+:: The path to 7zip "7z.exe"
 set "szPath=C:\Program Files\7-Zip\7z.exe"
 echo 	szPath = %szPath%
 
-:: Set this variable to true if you want to use WinRAR
+:: Set this variable to true if you want to use WinRA
 set useWinRAR=false
 
-:: The filename you wish to use for backup, you don't need to change this
-if %useWinRAR%==true set "fileName=%cd%HonourMode.rar"
-if %useWinRAR%==false set "fileName=%cd%HonourMode.7z"
+:: The filename you wish to use for backup, you don't need to change this unless you want to
+if %useWinRAR%==true (
+	set "fileName=%cd%HonourMode.rar"
+	set "fileBackup=%cd%HonourMode_PreviousSave.rar"
+) else (
+	set "fileName=%cd%HonourMode.7z"
+	set "fileBackup=%cd%HonourMode_PreviousSave.7z"
+)
 echo 	fileName = %fileName%
+echo 	fileBackup = %fileBackup%
 
 :MENU
 set input=false
 set "backupOrRestore=3"
 
 echo +===============================================+
-echo . BATCH SCRIPT - USER MENU                      .
+echo . Quick Save Backup Script                      .
 echo +===============================================+
 echo .                                               .
 echo .  1) BACKUP                                    .
@@ -49,8 +63,19 @@ if %input%==false exit /b
 :BACKUP
 
 cls
-echo Deleting %fileName% if it exists
-if exist %fileName% del %fileName%
+
+if %useCompression%==false (
+	echo Compression disabled, copying HonourMode.lsv and HonourMode.WebP to %cd%
+	copy "%backupDir%\HonourMode.lsv" "HonourMode.lsv"
+	copy "%backupDir%\HonourMode.WebP" "HonourMode.WebP"
+	goto MENU
+)
+
+if exist %fileName% (
+	echo Copying %fileName% to %fileBackup%
+	copy "%fileName%" "%fileBackup%"
+	del "%fileName%"
+)
 
 echo Compressing %backupDir% to %fileName%
 if %useWinRAR%==true (
@@ -64,10 +89,18 @@ goto MENU
 :RESTORE
 
 cls
+
+if %useCompression%==false (
+	echo Compression disabled, copying HonourMode.lsv and HonourMode.WebP to %backupDir%
+	copy "HonourMode.lsv" "%backupDir%\HonourMode.lsv"
+	copy "HonourMode.WebP" "%backupDir%\HonourMode.WebP"
+	goto MENU
+)
+
 if not exist %fileName% (
 	echo %fileName% does not exist, exiting
 	exit /b
-	)
+)
 	
 echo Extracting %fileName% to %backupDir% and overwriting
 if %useWinRAR%==true (
